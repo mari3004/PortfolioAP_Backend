8a0c44bb80902e4c88b613bc15e434d6d8465f2b
Maribel <mari.3004@hotmail.com>
2022-07-23 02:15:52 -0300

    upload 22.07

--- src/main/java/com/ar/maribelaizpitarte/backend/controller/PersonaController.java
+++ src/main/java/com/ar/maribelaizpitarte/backend/controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://frontend-portfolioap.web.app/#/")
+@CrossOrigin (origins = "https://frontend-portfolioap.web.app/")
 @RequestMapping("/api")
 public class PersonaController {
 
