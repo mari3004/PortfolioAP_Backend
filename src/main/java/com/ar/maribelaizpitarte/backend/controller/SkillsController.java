0827cbad4ee970e1c18716bf3c072105f542e6af
Maribel <mari.3004@hotmail.com>
2022-07-24 18:59:00 -0300

    upload 24.07

--- src/main/java/com/ar/maribelaizpitarte/backend/controller/SkillsController.java
+++ src/main/java/com/ar/maribelaizpitarte/backend/controller/SkillsController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin (origins = "https://apfrontend-2d469.web.app/skills")
+@RequestMapping("/api")
 public class SkillsController {
     
     @Autowired ISkillsService iskillsService;
