44596fee327f48742bdf9ec254b7e6c13eb83569
Maribel <mari.3004@hotmail.com>
2022-07-29 14:56:11 -0300

    correccion de errores

--- src/main/java/com/ar/maribelaizpitarte/backend/controller/PersonaController.java
+++ src/main/java/com/ar/maribelaizpitarte/backend/controller/PersonaController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin ("https://apfrontend-2d469.web.app/persona")
+@CrossOrigin ("https://apfrontend-2d469.web.app/")
 @RequestMapping("/api")
 public class PersonaController {
 
